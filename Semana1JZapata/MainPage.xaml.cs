61ac475d10393d5d2478cff1d7040e420f6b88e3
Jhomy Zapata <zapatajhomara95@gmail.com>
2021-04-30 21:21:57 -0500

    Agregar archivos de proyecto.

---
+++ Semana1JZapata/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana1JZapata
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private  void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+            string texto = txtMensaje.Text;
+            string mensaje = "Hola" + texto;
+            DisplayAlert("Este es un mensaje", mensaje, "ok");
+        }
+
+    }
+}
